d022b8d1273553511e33490894e9e471ac0c5cd5
Ömer Tunç <omertuncwho@gmail.com>
2024-02-04 00:08:46 +0300

    Update Sleep Debt Calculator

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 let button = document.querySelector("#calculate");
 
 function debtCalculator() {
-    let sleepHour = document.getElementById("aos");
-    let sleepHourMust = document.getElementById("aos-wget");
+    let recommendedHours = document.getElementById("recommended").value;
+    let actualHours = document.getElementById("actual").value;
     let resultElement = document.getElementById("result");
 
-    if (sleepHour.value >= 6) {
-        console.log(`Rest in peace..`);
-        resultElement.value = "REST IN PEACE";
+    if (recommendedHours >= 0 && actualHours >= 0) {
+        let sleepDebt = recommendedHours - actualHours;
+        if (sleepDebt <= 0) {
+            resultElement.value = "You are well-rested!";
+        } else {
+            resultElement.value = `Your sleep debt is ${sleepDebt} hours. Get some more rest!`;
+        }
     } else {
-        console.log(`Please enter valid data..`);
-        resultElement.value = ""; 
+        resultElement.value = "Please enter valid data.";
+    }
 }
 
 button.addEventListener("click", debtCalculator);
