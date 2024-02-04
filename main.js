3f10e5972221dc30abeea945b577d8757528c5c3
Ömer Tunç <omertuncwho@gmail.com>
2024-02-04 12:47:55 +0300

    Update Sleep Debt Calculator

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 let button = document.querySelector("#calculate");
+let resetButton = document.querySelector("#reset");
 
 function debtCalculator() {
-    let recommendedHours = document.getElementById("recommended").value;
-    let actualHours = document.getElementById("actual").value;
+    let recommendedHours = document.getElementById("recommended").valueAsNumber;
+    let actualHours = document.getElementById("actual").valueAsNumber;
     let resultElement = document.getElementById("result");
 
-    if (recommendedHours >= 0 && actualHours >= 0) {
+    if (!isNaN(recommendedHours) && !isNaN(actualHours) && recommendedHours >= 0 && actualHours >= 0) {
         let sleepDebt = recommendedHours - actualHours;
         if (sleepDebt <= 0) {
+            document.body.style.backgroundImage = "linear-gradient(to left, #f9f586, #96fbc4  )";
             resultElement.value = "You are well-rested!";
         } else {
+            document.body.style.backgroundImage = "linear-gradient(to left,  #ffb199,#ff0844)";
             resultElement.value = `Your sleep debt is ${sleepDebt} hours. Get some more rest!`;
         }
     } else {
+        document.body.style.backgroundImage = "linear-gradient(to left, #f6d365, #fda085)";
         resultElement.value = "Please enter valid data.";
     }
 }
 
+function resetCalculator() {
+    document.body.style.backgroundImage = "linear-gradient(to left, #FFFFFF, #FFEFBA)";
+    document.getElementById("recommended").value = "";
+    document.getElementById("actual").value = "";
+    document.getElementById("result").value = "";
+}
+
 button.addEventListener("click", debtCalculator);
+
